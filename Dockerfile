12636d35bec7a1077055206e20a930a43b9dd993
kiran pant <kiran.r.pant@gmail.com>
2024-03-29 19:55:06 +0545

    intro to docker and kubernetes

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM node:20
+WORKDIR /app
+COPY package.json .
+RUN npm install
+COPY  . .
+EXPOSE 4000
+CMD [ "node","app.mjs" ]
+
--- app.mjs
+++ app.mjs
@@ -1,6 +1,6 @@
 import express from "express";
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.get("/", (req, res) => {
   res.json({
